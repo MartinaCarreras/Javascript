eb5071a750576aa3aef9a6bdf6833f10ca2b6d3c
Martina Carreras <martina.carreras3@outlook.com>
2023-11-30 19:45:03 -0300

    1.0

---
+++ ejercicio-clase-1/Ejercicio 1/index.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt(`Ingrese el primer número`));
+let num2 = parseInt(prompt(`Ingrese un segundo número`));
+let resultado1 = num1 + num2;
+let resultado2 = num1 - num2;
+let resultado3 = num1 * num2;
+console.log(`Ambos números sumados es ${resultado1}`);
+console.log(`Ambos números restados es ${resultado2}`);
+console.log(`Ambos números multiplicados es ${resultado3}`);
